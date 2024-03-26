56959a26263e5e1c6d0447809da84b2bf6cd3f25
Vamshi Krishna <gvk556@gmail.com>
2024-03-26 19:57:20 +0530

    WhatsApp Business API first

--- main.py
+++ main.py
@@ -1,12 +1,94 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+import os
+import logging
+from heyoo import WhatsApp
 
 app = FastAPI()
+messenger = WhatsApp(os.getenv('ACCESS_TOKEN'), phone_number_id=os.getenv("PHONE_NUMBER_ID"))
 
+# Logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/hello")
-def read_item():
-    return {"custom": "Hey"}
+@app.get("/webhook")
+async def verifyToken(request: Request):
+    if request.query_params.get('hub.verify_token') == os.getenv('VERIFY_TOKEN'):
+        return Response(content=request.query_params.get('hub.challenge'))
+
+@app.post("/webhook")
+async def hook(request: Request):
+    # Handle Webhook Subscriptions
+    data = await request.json()
+
+    logging.info("Received webhook data: %s", data)
+    changed_field = messenger.changed_field(data)
+    if changed_field == "messages":
+        new_message = messenger.is_message(data)
+        if new_message:
+            mobile = messenger.get_mobile(data)
+            name = messenger.get_name(data)
+            message_type = messenger.get_message_type(data)
+            logging.info(
+                f"New Message; sender:{mobile} name:{name} type:{message_type}"
+            )
+            if message_type == "text":
+                message = messenger.get_message(data)
+                name = messenger.get_name(data)
+                logging.info("Message: %s", message)
+                messenger.send_message(f"Hi {name}, nice to connect with you", mobile)
+
+            elif message_type == "interactive":
+                message_response = messenger.get_interactive_response(data)
+                interactive_type = message_response.get("type")
+                message_id = message_response[interactive_type]["id"]
+                message_text = message_response[interactive_type]["title"]
+                logging.info(f"Interactive Message; {message_id}: {message_text}")
+
+            elif message_type == "location":
+                message_location = messenger.get_location(data)
+                message_latitude = message_location["latitude"]
+                message_longitude = message_location["longitude"]
+                logging.info("Location: %s, %s", message_latitude, message_longitude)
+
+            elif message_type == "image":
+                image = messenger.get_image(data)
+                image_id, mime_type = image["id"], image["mime_type"]
+                image_url = messenger.query_media_url(image_id)
+                image_filename = messenger.download_media(image_url, mime_type)
+                logging.info(f"{mobile} sent image {image_filename}")
+
+            elif message_type == "video":
+                video = messenger.get_video(data)
+                video_id, mime_type = video["id"], video["mime_type"]
+                video_url = messenger.query_media_url(video_id)
+                video_filename = messenger.download_media(video_url, mime_type)
+                logging.info(f"{mobile} sent video {video_filename}")
+
+            elif message_type == "audio":
+                audio = messenger.get_audio(data)
+                audio_id, mime_type = audio["id"], audio["mime_type"]
+                audio_url = messenger.query_media_url(audio_id)
+                audio_filename = messenger.download_media(audio_url, mime_type)
+                logging.info(f"{mobile} sent audio {audio_filename}")
+
+            elif message_type == "document":
+                file = messenger.get_document(data)
+                file_id, mime_type = file["id"], file["mime_type"]
+                file_url = messenger.query_media_url(file_id)
+                file_filename = messenger.download_media(file_url, mime_type)
+                logging.info(f"{mobile} sent file {file_filename}")
+            else:
+                logging.info(f"{mobile} sent {message_type} ")
+                logging.info(data)
+        else:
+            delivery = messenger.get_delivery(data)
+            if delivery:
+                logging.info(f"Message : {delivery}")
+            else:
+                logging.info("No new message")
+    return "OK", 200
